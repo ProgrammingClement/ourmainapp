1593af9caba3d4ebfee2790b8414f6ddd9d0328e
ProgrammingClement <programmingclement@gmail.com>
2023-02-20 20:00:56 -0800

    Just committing on my second commit I made

--- ourmainapp/app/Http/Controllers/ExampleController.php
+++ ourmainapp/app/Http/Controllers/ExampleController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class ExampleController extends Controller
 {
-    //
+    public function homepage() {
+        return view('homepage');
+    }
+
+    public function aboutPage() {
+        return '<h1>About Page!!!</h1><a href="/">Back to home</a>';
+    }
 }
